9865621f1610bbde9755bae55d428e7c3955715b
naru770 <natsumushi770@gmail.com>
2024-04-30 17:30:23 +0900

    リダイレクトを設定

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('node:http');
 const server = http.createServer((req, res) => {
+  res.writeHeader(302, {
+    Location: "https://nicovideo.jp/"
+  })
   res.end();
 });
 const port = 8000;
